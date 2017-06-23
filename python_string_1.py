26926abfaabf606a312a157c66888bd999f11624
karloffline <karl.lv@outlook.com>
2017-06-23 18:13:40 +0800

    no comment

--- python_string_1.py
+++ python_string_1.py
@@ -5,3 +5,15 @@
 # Karl.Lv@outlook.com, KarlLv@126.com
 #
 # Python String handling
+#
+
+str_ch_1 = '1234万'
+wang = '万'
+
+index = str_ch_1.index(wang)
+print(index)
+
+
+print(str_ch_1[:index]+'0000')
+
+
