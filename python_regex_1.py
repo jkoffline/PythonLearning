f738ec894e1dad6306153ec194465ecf4ada5593
karloffline <karl.lv@outlook.com>
2017-06-27 17:32:57 +0800

    Chinese characters

--- python_regex_1.py
+++ python_regex_1.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-#
+# -*- utf-8 -*-
 # 2 June, 2017
 #
 # Karl.Lv@outlook.com, KarlLv@126.com
 #
 # Python Regular Expression examples
 
+import re
 
-str_ch = '万'
+p2 = re.compile(r'万')
+str_ch_2 = '2318万'
+print(p2.sub(r'0000', str_ch_2))
 
-print(str_ch.encode('utf-8'))
+print('The UNICODE of 万 is:', hex(ord('万')))
+
+p3 = re.compile(r'\u4e07')
+str_ch_3 = '293万123万'
+print(p3.sub(r'0000', str_ch_3))
+
+str_ch_4 = '28938万万'
+if re.findall(r'万', str_ch_4):
+    print(re.sub(r'万', '0000', str_ch_4))
