f437bdbbe3b2367bae5a93d79ffbc10a1abc021a
karloffline <karl.lv@outlook.com>
2017-08-09 14:48:44 +0800

    nop

--- flask_app_4.py
+++ flask_app_4.py
@@ -11,8 +11,35 @@ from datetime import date
 import time
 
 from flask import Flask
+from flask import jsonify
+from flask import url_for
 
 app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_tasks', task_id=task['id'],
+                _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
 
 @app.route('/')
 def index():
@@ -20,6 +47,10 @@ def index():
         time.localtime(time.time()))
     str = "Hello Python3, it is " + now
     return str
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 if __name__ == '__main__':
     app.run(debug=True)
