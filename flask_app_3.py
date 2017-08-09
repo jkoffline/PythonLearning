12818f916008936eb3d8094dedbb0ec2bd9dfee6
karloffline <karl.lv@outlook.com>
2017-08-09 10:51:36 +0800

    A simple RESTful web service implemented by Flask

---
+++ flask_app_3.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+# -*- utf-8 -*-
+#
+# A simple RESTful web service implemented by Flask
+#
+# http://www.cnblogs.com/vovlie/p/4178077.html
+# https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+#
+# Karl.Lv@outlook.com, KarlLv@126.com
+# 09 August, 2017
+#
+
+"""
+To run this application:
+===============================================================================
+$ ./flask_app_3.py 
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 122-027-984
+===============================================================================
+    curl -i http://localhost:5000
+    curl -i http://localhost:5000/todo/api/v1.0/tasks/0
+    curl -i http://localhost:5000/todo/api/v1.0/tasks/1
+    curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:5000/todo/api/v1.0/tasks
+    curl -i -H "Content-Type: application/json" -X PUT -d '{"done":true}' http://localhost:5000/todo/api/v1.0/tasks/2
+    curl -i -H "Content-Type: application/json" -X DELETE -d '{"id":1}' http://localhost:5000/todo/api/v1.0/tasks/1
+    curl -i -H "Content-Type: application/json" -X DELETE http://localhost:5000/todo/api/v1.0/tasks/2
+"""
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import abort
+from flask import make_response
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_tasks(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+if __name__ == '__main__':
+    app.run(debug=True)
