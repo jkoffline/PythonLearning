8db1ede554015cc0c381d43c7032bd27bb0ff469
karloffline <karl.lv@outlook.com>
2017-08-18 09:16:25 +0800

    nop

--- python_urllib2_6.py
+++ python_urllib2_6.py
@@ -18,3 +18,14 @@ opener = urllib2.build_opener(handler)
 response = opener.open("http://www.sogou.com")
 
 cookie.save(ignore_discard=True, ignore_expires=True)
+
+
+cookie_2 = cookielib.MozillaCookieJar()
+
+cookie_2.load('python_urllib2_6.log', ignore_discard=True, ignore_expires=True)
+
+req = urllib2.Request("http://www.sogou.com")
+
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_2))
+response = opener.open(req)
+print response.read()
