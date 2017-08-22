bb52972eeb35d492b276eda4ecf583e4dd189c34
karloffline <karl.lv@outlook.com>
2017-08-22 11:08:23 +0800

    nop

---
+++ python_regex_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# Karl.Lv@outlook.com, KarlLv@126.com
+# 18 August, 2017
+
+import re
+
+m = re.match(r'(\w+) (\w+)', 'hello world!')
+
+print "m.string:", m.string
+print "m.re:", m.re
+print "m.pos:", m.pos
+print "m.endpos:", m.endpos
+print "m.lastindex:", m.lastindex
+print "m.lastgroup:", m.lastgroup
+print "m.group():", m.group()
+print "m.group(1,2):", m.group(1,2)
+print "m.groups():", m.groups()
+print "m.groupdict():", m.groupdict()
+print "m.start(2):", m.start(2)
+print "m.end(2):", m.end(2)
+print "m.span(2):", m.span(2)
